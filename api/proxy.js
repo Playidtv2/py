7e165cfbc329052e6b6aad18a1f4c2466ce2489e
ADMIN PLAYIDTV <159120249+Playidtv2@users.noreply.github.com>
2026-05-10 06:20:40 +0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -22,8 +22,8 @@ export default async function handler(req, res) {
       method: "GET",
       headers: {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-        "Referer": "https://warpdooball.net/",
-        "Origin": "https://warpdooball.net",
+        "Referer": "https://ballsod24hrs.com/",
+        "Origin": "https://ballsod24hrs.com",
         "Accept": "*/*"
       },
       signal: controller.signal
