6f52737b57417e035e93c87769d6ea494163463e
ADMIN PLAYIDTV <159120249+Playidtv2@users.noreply.github.com>
2026-06-01 12:48:54 +0700

    update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -8,8 +8,8 @@ export default async function handler(req, res) {
     const response = await fetch(target, {
       headers: {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
-        "Referer": "https://ballsod24hrs.com/",
-        "Origin": "https://ballsod24hrs.com"
+        "Referer": "https://ais-vidnt.com/",
+        "Origin": ""
       }
     });
 
